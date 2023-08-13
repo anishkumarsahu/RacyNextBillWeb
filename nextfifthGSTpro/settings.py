6803ea70dcd6bb019147bec8754da08ce45cb424
anishkumarsahu <anishkumarsahu03@gmail.com>
2023-08-13 14:35:29 +0530

    minor update

--- nextfifthGSTpro/settings.py
+++ nextfifthGSTpro/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nextbill',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '3306',
         'USER': 'root',
         'PASSWORD': 'password',
